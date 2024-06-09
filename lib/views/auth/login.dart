50fe8206372d0d5aed2184fc2e04d320be8868a1
yusuf <diandrayusuf23@gmail.com>
2024-06-09 22:25:21 +0700

    finish slicing login page

--- lib/views/auth/login.dart
+++ lib/views/auth/login.dart
@@ -1,6 +1,5 @@
 import 'package:dus_app/config/constant.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,12 +9,16 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+
+  bool isHide = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Constant.colorGreen,
       body: SafeArea(
-        child: Column(
+        child: ListView(
           children: [
             const SizedBox(
               height: 15,
@@ -47,28 +50,188 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(
               height: 5,
             ),
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Constant.colorWhite,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
+            Container(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height - 275,
+              ),
+              padding: const EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+                color: Constant.colorWhite,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
                 ),
-                child: ListView(
-                  padding: const EdgeInsets.all(30),
-                  children: const [
-                    Text(
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    width: double.maxFinite,
+                    child: Text(
                       'Login Dulu',
                       style: TextStyle(
                         fontSize: 28,
                         fontWeight: Constant.fontBold,
                       ),
                       textAlign: TextAlign.center,
-                    )
-                  ],
-                ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Text(
+                    'Email',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: Constant.fontSemiBold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Text(
+                    'Password',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: Constant.fontSemiBold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: passController,
+                    obscureText: isHide,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(
+                            () {
+                              isHide = !isHide;
+                            },
+                          );
+                        },
+                        icon: Icon(
+                          isHide ? Icons.visibility : Icons.visibility_off,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Constant.colorButton,
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 24,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Masuk',
+                        style: TextStyle(
+                          color: Constant.colorWhite,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          color: Constant.colorBlack,
+                          height: 1,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Text(
+                        'Atau',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: Container(
+                          color: Constant.colorBlack,
+                          height: 1,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Constant.colorWhite,
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 24,
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            side: const BorderSide(
+                              color: Constant.colorBlack,
+                            )),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'images/google.png',
+                            width: 28,
+                            height: 28,
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          const Text(
+                            'Masuk dengan Google',
+                            style: TextStyle(
+                              color: Constant.colorBlack,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
